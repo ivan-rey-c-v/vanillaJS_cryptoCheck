e5d706e1d213e2fd386f5b9645b10e1cc1d31049
ivan.rey.c.v <ivan.rey.c.v@gmail.com>
2018-03-07 15:12:44 +0800

    Add list items.

--- docs/master.js
+++ docs/master.js
@@ -2,16 +2,6 @@ function createNode (tag) {
 	return document.createElement(tag);
 }
 
-function addContent (node, content) {
-	node.textContent = content;
-	return node;
-}
-
-function append (parent, child) {
-	return parent.appendChild(child);
-}
-
-
 var _coins = ['ETH', 'DASH', 'BCH', 'BTC'];
 var _currency = ['USD', 'EUR'];
 
@@ -48,8 +38,54 @@ function getCoinsData () {
 	});
 }
 
-var _coinsData = getCoinsData();
+//var _coinsData = getCoinsData();
+
+function createSpan (content, className) {
+	let span = createNode('span');
+	span.textContent = content;
+	span.classList.add(className);
+	return span;
+}
+function createSpanImg (src) {
+	let span = createNode('span');
+	span.classList.add('flex-1');
+	let img = createNode('img');
+	img.setAttribute('src', src);
+	return span;
+}
+
+function createList (data) {
+	let li = createNode('li');
+	//let span1 = createSpanImg(data.imgUrl);
+	let span1 = createSpan('logo', 'flex-1');
+	let span2 = createSpan(data.rank, 'flex-1');
+	let span3 = createSpan(data.symbol, 'flex-1');
+	let span4 = createSpan(data.name, 'flex-2');
+	let span5 = createSpan(data.price, 'flex-3');
+	li.append(span1, span2, span3, span4, span5);
+	list.append(li);
+	return li;
+}
 
 function renderCoinsData(dataArray) {
 
 }
+
+createList(
+	{
+		imgUrl: 'asdf',
+		rank: 1,
+		symbol: 'ETH',
+		name: 'Ethereum',
+		price: 500
+	}
+)
+createList(
+	{
+		imgUrl: 'asdf',
+		rank: 1,
+		symbol: 'ETH',
+		name: 'Ethereum',
+		price: 500
+	}
+)
